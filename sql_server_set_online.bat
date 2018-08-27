e6a19df94f8fcbb0b843a2b09e0fd5e2d3bbd58c
nicholas-fink-phl <16352920+nicholas-fink-phl@users.noreply.github.com>
2018-08-27 14:23:43 -0400

    Update sql_server_set_online.bat

--- sql_server_set_online.bat
+++ sql_server_set_online.bat
@@ -1,3 +1,3 @@
-REM Sets SQL Server Instances online in the event that McAfee knocks them offline.
+REM Sets SQL Server instances online in the event that McAfee knocks them offline.
 echo off
 sqlcmd -S MACHINENAME\INSTANCE -i ~\set_online.sql
