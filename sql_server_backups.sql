9b818c0ce162d12972bed6dd1d30cf5cf2df1522
nicholas-fink-phl <16352920+nicholas-fink-phl@users.noreply.github.com>
2018-08-27 14:20:16 -0400

    Update sql_server_backups.sql

--- sql_server_backups.sql
+++ sql_server_backups.sql
@@ -1,3 +1,4 @@
+-- Creates backups for SQL Server Express, runs from sql_server_backups.bat
 DECLARE @MyFileName varchar(1000)
 SELECT @MyFileName = (SELECT "C:\Program Files\Microsoft SQL Server\MSSQL12.GIS_EDITING\MSSQL\Backup\TESTING_" + convert(varchar(500),GetDate(),112) + ".bak") 
 BACKUP DATABASE [TESTING] TO DISK=@MyFileName
