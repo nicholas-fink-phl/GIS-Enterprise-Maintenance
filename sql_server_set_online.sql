5afa54b6b86bc242437a41f96f097888e9780cc2
nicholas-fink-phl <16352920+nicholas-fink-phl@users.noreply.github.com>
2019-01-25 16:04:29 -0500

    Delete sql_server_set_online.sql

--- sql_server_set_online.sql
+++
@@ -1,7 +0,0 @@
--- sets SQL Server databases online if they've been knocked offline, runs from sql_server_set_online.bat
-alter database testing set online
-go
-alter database production set online
-go
-alter database ng_911_testing set online
-go
